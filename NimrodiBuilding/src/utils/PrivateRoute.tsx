b59bea15bdd6ad2a329be05f4c051eb97631d91f
ovadia-nisimov <ovadianisimov@gmail.com>
2024-11-07 11:25:04 +0200

    Added floor access check in PrivateRoute.tsx file

--- NimrodiBuilding/src/utils/PrivateRoute.tsx
+++ NimrodiBuilding/src/utils/PrivateRoute.tsx
@@ -1,13 +1,24 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router-dom";
+import { RootState } from "../store/store";
 
-interface IPrivateRoute{
-    component: ReactNode,
+interface IPrivateRoute {
+  component: ReactNode;
 }
 
-const PrivateRoute = ({ component}:IPrivateRoute) => {
-    //FILL HERE 3.6
+const PrivateRoute = ({ component }: IPrivateRoute) => {
+  const { index } = useParams<{ index: string }>();
+  const floorIndex = parseInt(index || "0");
+  const floorAccess = useSelector((state: RootState) => state.floorAccess.floorAccess);
+  const navigate = useNavigate();
+
+  if (!floorAccess[floorIndex]) {
+    navigate("/forbidden");
+    return null;
+  }
+
+  return <>{component}</>;
 };
 
-export default PrivateRoute
+export default PrivateRoute;
