c3bb769125b9efff909f02f26fa14a0411b455aa
ovadia-nisimov <ovadianisimov@gmail.com>
2024-11-07 10:35:13 +0200

    Setting the store of the reducers

--- NimrodiBuilding/src/store/store.ts
+++ NimrodiBuilding/src/store/store.ts
@@ -1,1 +1,14 @@
-// FILL HERE 3.3
+import { configureStore } from "@reduxjs/toolkit";
+import floorReducer from "./floorreducer";
+import roleReducer from "./rolereducer";
+
+const store = configureStore({
+  reducer: {
+    floorAccess: floorReducer,
+    role: roleReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
