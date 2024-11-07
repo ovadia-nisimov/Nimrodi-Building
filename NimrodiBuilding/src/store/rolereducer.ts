5dbd59eb92a4d500146059b8272d75a2bac38e7e
ovadia-nisimov <ovadianisimov@gmail.com>
2024-11-07 10:29:58 +0200

    Added Rolereducer.ts according to role.json

--- NimrodiBuilding/src/store/rolereducer.ts
+++ NimrodiBuilding/src/store/rolereducer.ts
@@ -1,1 +1,23 @@
-//FILL HERE 3.2
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import roles from "../data/roles.json";
+
+interface RoleState {
+  role: string;
+}
+
+const initialState: RoleState = {
+  role: roles[0], 
+};
+
+const roleSlice = createSlice({
+  name: "role",
+  initialState,
+  reducers: {
+    setRole: (state, action: PayloadAction<number>) => {
+      state.role = roles[action.payload];
+    },
+  },
+});
+
+export const { setRole } = roleSlice.actions;
+export default roleSlice.reducer;
